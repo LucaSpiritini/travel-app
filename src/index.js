5da4c44f70167157da71e3b1ae43b5b8ebb6e3da
Luca Spiritini <luca.spiritini@hotmail.com>
2022-06-07 18:35:10 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//Otra forma de hacer lo mismo
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <App />
+// );
+
